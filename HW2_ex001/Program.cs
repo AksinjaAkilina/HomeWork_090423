3d0f23ceca56abb6f49b25134a894f75f0aa9d32
Akilina Ksenia <ksusysol@gmail.com>
2023-04-15 16:48:17 +0400

    Добавлена HM2 ex001

---
+++ HW2_ex001/Program.cs
@@ -0,0 +1,11 @@
+﻿int random = new Random().Next(100, 1000);
+Console.WriteLine(random);
+
+Console.WriteLine(FindSecond(random));
+int FindSecond(int random)
+{
+    int result;
+    result = random % 100 / 10;
+    return result;
+}
+
