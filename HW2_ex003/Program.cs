eb0db08e084c589122df4c261120067b6590eed4
Akilina Ksenia <ksusysol@gmail.com>
2023-04-15 20:37:03 +0400

    Добавлена HM2 ex003

---
+++ HW2_ex003/Program.cs
@@ -0,0 +1,19 @@
+﻿int day = ReadInt("Введите число от 1 до 7: ");
+int ReadInt(string number)
+{
+    Console.Write(number);
+    int i = 0;
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        Console.WriteLine("It's not a number");
+    }
+    return i;
+}
+
+if (day >= 1 && day <= 7)
+{
+    if (day == 6 | day == 7) Console.WriteLine("Сегодня выходной");
+    else Console.WriteLine("Сегодня рабочий день");
+}
+else Console.WriteLine("Не верное число");
+
