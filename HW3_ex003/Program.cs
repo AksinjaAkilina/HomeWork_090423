f66008741bdddd9b4503cd8646ac37c8c6a0f88f
Akilina Ksenia <ksusysol@gmail.com>
2023-04-16 23:22:24 +0400

    Добавлена HM3 ex003

---
+++ HW3_ex003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int x = ReadInt("Введите число: ");
+System.Console.WriteLine(string.Join(" ", Cube(x)));
+
+int ReadInt(string argument)
+{
+  Console.Write(argument);
+
+  int i;
+
+  while (!int.TryParse(Console.ReadLine(), out i))
+  {
+    Console.WriteLine("It's not a number");
+  }
+
+  return i;
+}
+
+int [] Cube(int x)
+{
+        int [] Positive = new int [x];
+        for (int i = 0; i < x; i++)
+        {
+            Positive[i] = (int)Math.Pow(i + 1, 3); 
+        }
+    
+        return Positive;   
+}
