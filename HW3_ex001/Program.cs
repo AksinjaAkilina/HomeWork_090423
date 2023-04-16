232127407fdc14cfbc91d74b59000d87bc469488
Akilina Ksenia <ksusysol@gmail.com>
2023-04-16 22:46:26 +0400

    исправлена HM3 ex001

--- HW3_ex001/Program.cs
+++ HW3_ex001/Program.cs
@@ -1,23 +1,58 @@
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+int x = ReadInt("Введите пятизначное число: ");
+int ReadInt(string argument)
+{
+	Console.Write(argument);
 
-Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-
-int length = number.Length;
+	int i;
 
 	while (!int.TryParse(Console.ReadLine(), out i))
 	{
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine($"{number} - Палиндром");
-    }
-    else
-    {
-        Console.WriteLine($"{number} - НЕ палиндром");
-    }
+        Console.WriteLine("Это не число!");
+        Console.Write(argument);
+        
+	}
+
+	return i;
+}
+
+
+if (IsPalindrome(M(x)))
+{
+    System.Console.WriteLine("Это число палиндром");
 }
 else
 {
-    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+    System.Console.WriteLine("Это число не палиндром");
+}
+
+int [] M(int x)
+{
+    string str = x.ToString();
+    int [] S = new int [str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+       S[i] = int.Parse(str[i].ToString()); 
+    }
+    return S;
+}
+
+bool IsPalindrome(int [] S)
+{  
+    int i = 0;
+    int k = S.Length - 1;
+    while (i < k)
+    {
+        if (S[i] == S[k])
+        {
+            i++;
+            k--;
+        }
+        else
+        {
+            return false;
+        }
+    }
+return true;
 }
